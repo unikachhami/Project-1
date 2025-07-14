d410b772f544260ab4c6dde537ed294fd344e8f0
Unik <unikbahadur1852@gmail.com>
2025-07-14 15:58:11 +0545

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", 
+    version="0.0.1",
+    author="Unik Achhami",
+    author_email="unikbahadur1852@gmail.com",
+    packages=find_packages(),
+    
+)
